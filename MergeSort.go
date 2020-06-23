557ea447a697c3f407be023a79515a46652fcd04
Jehan Tremback <jehan@MacBook-Pro.webpass.net>
2020-06-23 11:20:27 -0700

    mergesort is n log n

--- MergeSort.go
+++ MergeSort.go
@@ -2,12 +2,14 @@ package main
 
 // arr: |...| |  | |  |...|
 //          i    j    n
-func Merge(arr []int, aux []int, lo int, mid int, hi int) {
+func Merge(arr []int, aux []int, lo int, mid int, hi int) int {
+	ops := 0
 	i := lo
 	j := mid
 	l := hi - lo
 	// Iterate while there are still items to move to aux
 	for k := 0; k < l; k++ {
+		ops++
 		if i >= mid {
 			// If there are no items remaining on the left, but there are some on the right
 			aux[k] = arr[j]
@@ -28,17 +30,22 @@ func Merge(arr []int, aux []int, lo int, mid int, hi int) {
 	}
 	// copy items in aux array back into the main array
 	for i := 0; i < l; i++ {
+		ops++
 		arr[lo+i] = aux[i]
 	}
+	return ops
 }
 
-func MergeSort(arr []int) {
+func MergeSort(arr []int) int {
+	ops := 0
 	l := len(arr)
 	aux := make([]int, l)
 	interval := 1
 	for interval < l {
+		ops++
 		lo := 0
 		for lo < l {
+			ops++
 			mid := lo + interval
 			hi := mid + interval
 
@@ -46,9 +53,11 @@ func MergeSort(arr []int) {
 				hi = l
 			}
 
-			Merge(arr, aux, lo, mid, hi)
+			ops += Merge(arr, aux, lo, mid, hi)
 			lo = hi
 		}
 		interval = interval * 2
 	}
+
+	return ops
 }
--- MergeSort_test.go
+++ MergeSort_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -9,4 +10,34 @@ func TestMergeSort(t *testing.T) {
 	arr := []int{11, 10, 1, 9, 8, 7, 4, 6, 5, 2, 4, 3, 2, 1}
 	MergeSort(arr)
 	fmt.Printf("%#v\n", arr)
+}
+
+func TestFuzzMergeSort(t *testing.T) {
+	trials := []int{10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000}
+	for i := 0; i < len(trials); i++ {
+		arr := generateRandomSlice(trials[i], 99999999)
+		ops := MergeSort(arr)
+		if !isSorted(arr) {
+			panic("not sorted")
+		}
+		fmt.Println("Length: ", trials[i], "Ops: ", ops, "Ratio: ", ops/trials[i])
+	}
+}
+
+func generateRandomSlice(size int, rnge int) []int {
+	slice := make([]int, size, size)
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(rnge) - rand.Intn(rnge)
+	}
+	return slice
+}
+
+func isSorted(arr []int) bool {
+	for i := 1; i < len(arr); i++ {
+		// If the current item is lower than the previous one
+		if arr[i] < arr[i-1] {
+			return false
+		}
+	}
+	return true
 }
