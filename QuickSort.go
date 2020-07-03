1d51ba6818a6901cf7f2610b31fae5266b2fdedc
Jehan Tremback <jehan@MacBook-Pro.webpass.net>
2020-07-03 13:25:00 -0700

    more partition code and good display method

--- QuickSort.go
+++ QuickSort.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
+// If a[i] is not greater than the pivot, move i up one.
+// If a[i] is greater than the pivot, switch it with a[j], and move j down one
+// If a[j] is smaller than the pivot, switch it with a[i], and move i up one ?????
+
 func Partition(a []int) []int {
 	pivot := a[0]
 	i := 1
 	j := len(a) - 1
 	for {
-		checkNotGreaterThan(a[1:i], pivot)
-		checkNotLessThan(a[j:], pivot)
+		printArray(a, i, j)
+		// checkNotGreaterThan(a[1:i], pivot)
+		// checkNotLessThan(a[j:], pivot)
 		if a[i] > pivot {
 			item := a[i]
 			a[i] = a[j]
@@ -18,6 +23,22 @@ func Partition(a []int) []int {
 			i++
 		}
 	}
+}
+
+func printArray(a []int, i int, j int) {
+	fmt.Printf("%+v\n", a)
+	pointers := ""
+	for k := 0; k < len(a)+2; k++ {
+		if k == i {
+			pointers += " i"
+		} else if k == j {
+			pointers += " j"
+		} else {
+			pointers += "  "
+		}
+
+	}
+	println(pointers)
 }
 
 func checkNotGreaterThan(a []int, pivot int) {
--- QuickSort_test.go
+++ QuickSort_test.go
@@ -5,6 +5,6 @@ import (
 )
 
 func TestQSPartition(t *testing.T) {
-	a := []int{1, 3, 1, 4, 5, 8, 2, 2, 1, 5, 4, 6}
+	a := []int{5, 3, 1, 4, 5, 8, 2, 2, 1, 5, 4, 6}
 	println(Partition(a))
 }
